0a5c0ebed05940e387c8aef9a64b0db835ca35f0
Matt <mattwalker94@hotmail.com>
2025-11-20 19:02:42 +0000

    Phase 2: Added static pages and navbar with navigation

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/feed")
+def feed():
+    return render_template("feed.html")
+
+@app.route("/add_gig")
+def add_gig():
+    return render_template("add_gig.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
